a8ae415fb3e6d365426b7c33ed866c6265d51236
Halil Eren Öymez <erenhalil2010@hotmail.com>
2024-12-16 15:55:49 +0300

    iteration increase

--- ADAProject.py
+++ ADAProject.py
@@ -624,7 +624,7 @@ y = df['y']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize the Logistic Regression model with balanced class weights
-model = LogisticRegression(class_weight='balanced', max_iter=100, solver='liblinear')
+model = LogisticRegression(class_weight='balanced', max_iter=200, solver='liblinear')
 
 # Fit the model
 model.fit(X_train, y_train)
