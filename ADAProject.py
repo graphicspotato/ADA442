f387581f9b2543ca15d941df0ab04f1a1d32987d
Halil Eren Öymez <erenhalil2010@hotmail.com>
2024-12-17 21:54:08 +0300

    menu test

--- ADAProject.py
+++ ADAProject.py
@@ -18,16 +18,16 @@ def load_lottie_file(filepath: str):
     with open(filepath, 'r') as f:
         return json.load(f)
     
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def load_model():
     with st.spinner("Loading model..."):
         return joblib.load(model_path)
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def load_data():
     return pd.read_csv(data_path)
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def load_animation_m():
     return load_lottie_file(machine_json_path)
 
@@ -68,7 +68,7 @@ data= pd.read_csv(data_path)
 
 # Page selection using sidebar
 st.sidebar.title("Page Navigator")
-page = st.sidebar.selectbox("Choose a page", ["Home", "Data Exploration","Data Processing","Data Visualization", "Machine Learning Prediction Model","Feedback"])
+page = st.sidebar.selectbox("Choose a page", ["Home", "Data Exploration","Data Visualization", "Data Processing", "Machine Learning Prediction Model","Feedback"])
 
 if page == "Home":
    st.title("Welcome to the Bank Marketing Campaign Prediction App")
@@ -80,13 +80,25 @@ if page == "Home":
             and input the required features to get a prediction.
         """)
 elif page == "Data Exploration":
-    st.title("About Our Dataset")
+    st.title("Let's Explore Our Dataset")
     st.write("The data is related with direct marketing campaigns (phone calls) of a Portuguese banking institution. The classification goal is to predict if the client will subscribe a term deposit (variable y).")
     st.write("Additional Information:")
     st.write(" The marketing campaigns were based on phone calls. Often, more than one contact to the same client was required, in order to access if the product (bank term deposit) would be ('yes') or not ('no') subscribed.")
     st.write("There are four datasets which can be used but our work based on bank-additional.csv with 10% of the examples (4119), randomly selected from 1), and 20 inputs.")
     st.write("Luckily We don't have any missing values in our dataset so it seems there is no need to data imputation.")
     
+elif page == "Data Visualization":
+    st.title("Data Visualization")
+    st.write("In this page you can visualise the general information of our dataset.")
+    num_rows = st.number_input("Select number of rows to view", min_value=5, max_value=50, value=10)
+     # Show the first few rows of the data set
+    st.write(f"Here is a preview of the first {num_rows} rows of the dataset:")
+    st.write(data.head(num_rows))
+    st.write(feature_info)
+    st.subheader("Null Values in Each Column")
+    null_values = df.isnull().sum()
+    # Display null values as a table
+    st.write(null_values)
 
     
     
